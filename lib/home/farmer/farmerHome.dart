1363e49e48802543f0356a364a4daf4b4cbbfb65
prasanjitha <graphicdis126@gmail.com>
2021-08-25 07:54:24 +0530

    created farmer home page

---
+++ lib/home/farmer/farmerHome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class faemerHome extends StatefulWidget {
+  const faemerHome({Key? key}) : super(key: key);
+
+  @override
+  _faemerHomeState createState() => _faemerHomeState();
+}
+
+class _faemerHomeState extends State<faemerHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
