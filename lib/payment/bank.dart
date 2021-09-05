d3a9c9f4eda397beae7e98b89da6acc17f3cf2d4
prasanjitha <graphicdis126@gmail.com>
2021-09-05 15:44:40 +0530

    created payment method

---
+++ lib/payment/bank.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class bankPayment extends StatefulWidget {
+  const bankPayment({Key? key}) : super(key: key);
+
+  @override
+  _bankPaymentState createState() => _bankPaymentState();
+}
+
+class _bankPaymentState extends State<bankPayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Your Payment"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
