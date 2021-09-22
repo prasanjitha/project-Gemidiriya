5eac273dbd9b7763b63234a2743c215af8c4cd2e
prasanjitha <graphicdis126@gmail.com>
2021-09-22 20:48:01 +0530

    add new sicurity method

---
+++ lib/home/security.dart
@@ -0,0 +1,13 @@
+class security extends StatefulWidget {
+  const security({Key? key}) : super(key: key);
+
+  @override
+  _securityState createState() => _securityState();
+}
+
+class _securityState extends State<security> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
