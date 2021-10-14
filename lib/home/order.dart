b1d94e84b72c699deff3e53944f69d7c40f27c3d
prasanjitha <graphicdis126@gmail.com>
2021-10-14 20:59:39 +0530

    add new home.dart file

--- lib/home/order.dart
+++ lib/home/order.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scafflod();
+  }
+}
--- lib/home/security.dart
+++ lib/home/security.dart
@@ -8,6 +8,10 @@ class security extends StatefulWidget {
 class _securityState extends State<security> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appbar: AppBar("news"), body: Container());
+    return Scaffold(
+        appbar: AppBar("news"),
+        body: Container(
+          child: Text("Home"),
+        ));
   }
 }
