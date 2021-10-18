c02b670023ddba2196419727aac4f11356d159e9
prasanjitha <graphicdis126@gmail.com>
2021-10-18 22:15:46 +0530

    add new dependency

--- lib/home/security.dart
+++ lib/home/security.dart
@@ -11,7 +11,7 @@ class _securityState extends State<security> {
     return Scaffold(
         appbar: AppBar("news"),
         body: Container(
-          child: Text("Home"),
+          child: Text("Home for bulid"),
         ));
   }
 }
