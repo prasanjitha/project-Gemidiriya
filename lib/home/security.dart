68d130ebb773144e6e8d5c9e6d49ad23e2d65d1d
prasanjitha <graphicdis126@gmail.com>
2021-11-22 15:20:39 +0530

    add new library

--- lib/home/security.dart
+++ lib/home/security.dart
@@ -9,7 +9,7 @@ class _securityState extends State<security> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appbar: AppBar("news app screen for home add"),
+        appbar: AppBar("news app screen for home add someone"),
         body: Container(
           child: Text("Home is bulid by bass good ones add new thing"),
         ));
