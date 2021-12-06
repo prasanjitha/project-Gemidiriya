8f0cee37b60d79e5904c658f7e01ff8c843c5931
prasanjitha <graphicdis126@gmail.com>
2021-12-07 01:00:47 +0530

    add new directly

--- lib/home/security.dart
+++ lib/home/security.dart
@@ -9,7 +9,7 @@ class _securityState extends State<security> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appbar: AppBar("news app screen for home add someone"),
+        appbar: AppBar("news app screen for home add someone else"),
         body: Container(
           child: Text(
               "Home is bulid by bass good ones add the new thing is added. It's better. Yes it is. yes ok"),
