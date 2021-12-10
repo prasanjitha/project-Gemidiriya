8143f32baa75d2abc25d80ca0fea5c17f70083b5
prasanjitha <graphicdis126@gmail.com>
2021-12-10 20:16:23 +0530

    change app bar name

--- lib/home/security.dart
+++ lib/home/security.dart
@@ -9,7 +9,7 @@ class _securityState extends State<security> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appbar: AppBar("news app screen for home add someone else good"),
+        appbar: AppBar("news app screen for home add someone else old is  good"),
         body: Container(
           child: Text(
               "Home is bulid by bass good ones add the new thing is added. It's better. Yes it is. yes ok"),
