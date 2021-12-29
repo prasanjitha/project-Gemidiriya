d176607bf4bd16c8a9244da8ffca7535bfac7192
prasanjitha <graphicdis126@gmail.com>
2021-12-29 19:11:44 +0530

    eddit app bar name

--- lib/home/security.dart
+++ lib/home/security.dart
@@ -9,7 +9,8 @@ class _securityState extends State<security> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appbar: AppBar("news app screen for home add new someone else old is  good for me create new life good"),
+        appbar: AppBar(
+            "news app screen for home add new age someone else old is  good for me create new life good"),
         body: Container(
           child: Text(
               "Home is bulid by bass good ones add the good thing new thing is added. It's better. Yes it is. yes ok"),
